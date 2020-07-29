43206a578619bec010610b2facaa2a233a5c3a48
kalashnikovsb <kalashnikov.sk8@gmail.com>
2020-07-29 23:16:22 +0700

    Выполнил задание 3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 'use strict';
 
+const EVENTS_COUNT = 3;
+
 const tripHeader = document.querySelector(`.trip-main`);
-const tripHeaderControls = tripHeader.querySelector(`.trip-controls`);
-const tripHeaderCaptions = tripHeaderControls.querySelectorAll(`.trip-controls h2`);
+const tripHeaderMenu = tripHeader.querySelector(`.trip-controls`);
+const tripHeaderCaptions = tripHeaderMenu.querySelectorAll(`.trip-controls h2`);
 const tripEvents = document.querySelector(`.trip-events`);
 
 const createTripInfo = () => {
@@ -262,6 +264,64 @@ const createEventWithDestination = () => {
     </form>`
   );
 };
+const createDaysList = () => {
+  return (
+    `<ul class="trip-days"></ul>`
+  );
+};
+const createDayItem = () => {
+  return (
+    `<li class="trip-days__item  day">
+      <div class="day__info">
+        <span class="day__counter">1</span>
+        <time class="day__date" datetime="2019-03-18">MAR 18</time>
+      </div>
+    </li>`
+  );
+};
+const createEventsList = () => {
+  return (
+    `<ul class="trip-events__list"></ul>`
+  );
+};
+const createEventItem = () => {
+  return (
+    `<li class="trip-events__item">
+      <div class="event">
+        <div class="event__type">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+        </div>
+        <h3 class="event__title">Taxi to Amsterdam</h3>
+
+        <div class="event__schedule">
+          <p class="event__time">
+            <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+            &mdash;
+            <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+          </p>
+          <p class="event__duration">30M</p>
+        </div>
+
+        <p class="event__price">
+          &euro;&nbsp;<span class="event__price-value">20</span>
+        </p>
+
+        <h4 class="visually-hidden">Offers:</h4>
+        <ul class="event__selected-offers">
+          <li class="event__offer">
+            <span class="event__offer-title">Order Uber</span>
+            &plus;
+            &euro;&nbsp;<span class="event__offer-price">20</span>
+           </li>
+        </ul>
+
+        <button class="event__rollup-btn" type="button">
+          <span class="visually-hidden">Open event</span>
+        </button>
+      </div>
+    </li>`
+  );
+};
 
 const render = (container, template, position) => {
   container.insertAdjacentHTML(position, template);
@@ -272,3 +332,18 @@ render(tripHeaderCaptions[0], createHeaderMenu(), `afterend`);
 render(tripHeaderCaptions[1], createTimeFilter(), `afterend`);
 render(tripEvents, createSorting(), `beforeend`);
 render(tripEvents, createEventWithDestination(), `beforeend`);
+render(tripEvents, createDaysList(), `beforeend`);
+
+const daysList = tripEvents.querySelector(`.trip-days`);
+
+render(daysList, createDayItem(), `beforeend`);
+
+const day = daysList.querySelector(`.day`);
+
+render(day, createEventsList(), `beforeend`);
+
+const eventsList = day.querySelector(`.trip-events__list`);
+
+for (let i = 0; i < EVENTS_COUNT; i++) {
+  render(eventsList, createEventItem(), `beforeend`);
+}
