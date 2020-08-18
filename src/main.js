12a00b85db81dcfa65962b9f3754d2850bf206f3
kalashnikovsb <kalashnikov.sk8@gmail.com>
2020-08-18 13:09:33 +0700

    Убрал закоментированный кот

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import TripInfo from "./view/TripInfo.js";
 import HeaderMenu from "./view/HeaderMenu.js";
 import Filter from "./view/Filter.js";
 import Sorting from "./view/Sorting.js";
-// import EventWithDestination from "./view/EventWithDestination.js";
 import DaysList from "./view/DaysList.js";
 import DayItem from "./view/DayItem.js";
 import EventsList from "./view/EventsList.js";
@@ -24,19 +23,14 @@ const eventsSortByTime = (first, second) => {
 };
 
 const getObjectDatesList = (arrayOfEvents) => {
-  // Создаю объект, у которого ключи это уникальные даты событий
   let dates = {};
   arrayOfEvents.forEach((item) => {
     const [year, month, day] = parseTimeToArray(item.timeStart);
     const key = `${year}-${month}-${day}`;
-    // Присваиваю каждому ключу пустой массив, в который буду класть события
     dates[key] = [];
   });
-  // Прохожу все ключи
   for (let key in dates) {
-    // Чтобы линтер не ругался
     if (typeof key === `string`) {
-      // Фильтрую массив событий
       dates[key] = arrayOfEvents.filter((item) => {
         const [year, month, day] = parseTimeToArray(item.timeStart);
         return (key === `${year}-${month}-${day}`);
@@ -46,41 +40,30 @@ const getObjectDatesList = (arrayOfEvents) => {
   return dates;
 };
 
-
-// Генерирую события и сортирую по времени
 const events = new Array(EVENTS_COUNT).fill().map(generateEvent).sort(eventsSortByTime);
-// Созаю объект дат, с массивами событий для каждой даты
 const objectDates = getObjectDatesList(events);
-
 
 renderElement(tripHeader, new TripInfo(events).getElement(), renderPosition.AFTERBEGIN);
 renderElement(tripHeaderCaptions[0], new HeaderMenu().getElement(), renderPosition.AFTEREND);
 renderElement(tripHeaderCaptions[1], new Filter().getElement(), renderPosition.AFTEREND);
 renderElement(tripEvents, new Sorting().getElement(), renderPosition.BEFOREEND);
-// renderElement(tripEvents, new EventWithDestination(events[0]).getElement(), renderPosition.BEFOREEND);
 renderElement(tripEvents, new DaysList().getElement(), renderPosition.BEFOREEND);
 
 const daysList = tripEvents.querySelector(`.trip-days`);
 
-
-// Счетчик дней в путешествии
 let dayNumber = 1;
 const objectDateKeys = Object.keys(objectDates);
 for (let key of objectDateKeys) {
-  // Передаю счетчик и дату для отображения блока
   renderElement(daysList, new DayItem(dayNumber, key).getElement(), renderPosition.BEFOREEND);
   dayNumber++;
 
-  // Коллекция всех дней
   const allDays = daysList.querySelectorAll(`.day`);
-  // Последний день в коллекции, чтобы вставить именно в него события
   const day = allDays[allDays.length - 1];
 
   renderElement(day, new EventsList().getElement(), renderPosition.BEFOREEND);
   const eventsList = day.querySelector(`.trip-events__list`);
 
   for (const event of objectDates[key]) {
-    // Создаю события сразу в 2 видах - обычном и редактируемом. Так же нахожу кнопки открыть/закрыть
     const usualEvent = new EventItem(event).getElement();
     const openButton = usualEvent.querySelector(`.event__rollup-btn`);
     const editingEvent = new EventEdit(event).getElement();
